ed579f1651b53f529c0f15a843bc7f41068c5614
BenjaminAaron <benjaminaaron.only@gmail.com>
2024-09-05 14:26:29 +0800

    使用函数编写

--- main.c
+++ main.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 
-int main(void) {
+void class4q3(void) {
     int count = 0;
     for (int i = 1; i < 40; i++) {
         for (int j = 1; j < 40; j++) {
             for (int m = 1; m < 40; m++) {
                 for (int n = 1; n < 40; n++) {
-                    if ((i+j+m+n)==40) {
-                        if ((i*10+j*5+m*2+n)==100)
+                    if ((i + j + m + n) == 40) {
+                        if ((i * 10 + j * 5 + m * 2 + n) == 100)
                             count++;
                     }
                 }
@@ -15,6 +15,9 @@ int main(void) {
         }
     }
     printf("%d", count);
+}
 
+int main(void) {
+    class4q3();
     return 0;
 }
