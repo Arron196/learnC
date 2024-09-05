7ca11e02a07b3d63c08f63b5bd924cd5bc3c9bc7
BenjaminAaron <benjaminaaron.only@gmail.com>
2024-09-05 14:17:04 +0800

    课时4问题3

---
+++ main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(void) {
+    int count = 0;
+    for (int i = 1; i < 40; i++) {
+        for (int j = 1; j < 40; j++) {
+            for (int m = 1; m < 40; m++) {
+                for (int n = 1; n < 40; n++) {
+                    if ((i+j+m+n)==40) {
+                        if ((i*10+j*5+m*2+n)==100)
+                            count++;
+                    }
+                }
+            }
+        }
+    }
+    printf("%d", count);
+
+    return 0;
+}
